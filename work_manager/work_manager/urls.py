8de79a9b63d288129d013d55075cf12bdc687fb5
Mauro Dattilo <mau.dattilo@gmail.com>
2015-02-02 21:34:35 +0100

    Add index 'Hello World' view

--- work_manager/work_manager/urls.py
+++ work_manager/work_manager/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'work_manager.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'tasksmanager.views.page'),
+    url(r'^index$', 'tasksmanager.views.page'),
     url(r'^admin/', include(admin.site.urls)),
 )
