4f0e60de2acc70d006d3086f4d02907610e3d9dc
Mauro Dattilo <mau.dattilo@gmail.com>
2015-02-03 18:24:57 +0100

    Add create developer page

--- work_manager/tasksmanager/views.py
+++ work_manager/tasksmanager/views.py
@@ -1,7 +1,15 @@
+from django import forms
 from django.shortcuts import render
+from django.http import HttpResponse
 
-from .models import Project
+from .models import Project, Supervisor, Developer
 
+
+class form_inscription(forms.Form):
+    name = forms.CharField(label="Name", max_length=30)
+    login = forms.CharField(label="Login", max_length=30)
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    supervisor = forms.ModelChoiceField(label="Supervisor", queryset=Supervisor.objects.all())
 
 def index(request):
     action = 'Display project with client name = "Me"'
@@ -13,5 +21,22 @@ def project_detail(request, pk):
     project = Project.objects.get(id=pk)
     return render(request, 'en/public/project_detail.html', {'project': project})
 
+def create_developer(request):
+    if request.POST:
+        form = form_inscription(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            login = form.cleaned_data['login']
+            password = form.cleaned_data['password']
+            supervisor = form.cleaned_data['supervisor']
+            new_developer = Developer(name=name, login=login, password=password,
+                                      email="", supervisor=supervisor)
+            new_developer.save()
+            return HttpResponse("Developer added")
+        else:
+            return render(request, 'en/public/create_developer.html', {'form': form})
+    else:
+        form = form_inscription()
+        return render(request, 'en/public/create_developer.html', {'form': form})
 def connection(request):
     return render(request, 'en/public/connection.html')
--- work_manager/work_manager/urls.py
+++ work_manager/work_manager/urls.py
@@ -8,8 +8,10 @@ urlpatterns = patterns('',
     # url(r'^$', 'work_manager.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+
     url(r'^$', 'tasksmanager.views.index', name="public_index"),
     url(r'^connection$', 'tasksmanager.views.connection', name="public_connection"),
     url(r'^project-detail-(?P<pk>\d+)$', 'tasksmanager.views.project_detail', name="project_detail"),
+    url(r'^create-developer$', 'tasksmanager.views.create_developer', name="create_developer"),
     url(r'^admin/', include(admin.site.urls)),
 )
